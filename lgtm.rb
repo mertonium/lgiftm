62f8bd1c1b796970a5b070c986c6da2886033f47
John Mertens <john.mertens@gmail.com>
2017-11-27 15:42:05 -0800

    Take functionality out of view, put in controller

--- lgtm.rb
+++ lgtm.rb
@@ -1,8 +1,8 @@
 require 'sinatra'
 require 'haml'
 
-LGTM_URLS = IO.readlines('data/lgtm_urls.txt').map(&:strip)
+LGTM_URLS = IO.readlines('data/lgtm_urls.txt').map(&:strip).uniq
 
 get '/' do
-  haml :index, locals: { urls: LGTM_URLS }
+  haml :index, locals: { urls: LGTM_URLS.shuffle }
 end
